ef6f1b846bb56b7d625755d62e674bf09e506afe
Nathan Kerr <nathankerr@gmail.com>
2014-12-20 16:25:43 -0800

    Update to log, spec not working correctly.

--- test/spec/firefoxos.spec.js
+++ test/spec/firefoxos.spec.js
@@ -14,10 +14,24 @@ describe('base api function', function () {
 	});
 
 	it('should return nothing', function () {
-		var result = ajax('http://mozilla.org', null, function(d,s,x){
-			console.log(s);
+		var result = ajax('http://mozilla.org', null, function (d, s, x) {
 		});
 		expect(result).toBeUndefined();
 	});
+});
 
+ddescribe('makes async requests', function () {
+	var results;
+	beforeEach(function (done) {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+		results = null;
+		ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result) {
+			results = result;
+			console.log(result);
+			done();
+		});
+	});
+	it('should return something', function () {
+		expect(results).not.toBeUndefined();
+	});
 });
