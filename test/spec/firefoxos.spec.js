6ee19a619a9a7065c2075246e0c3330a67c06bc0
Casey English <kcpisme@gmail.com>
2014-12-20 23:36:05 -0800

    Tried to tackle CORS error-handling. Added an xhr.onerror() function.

--- test/spec/firefoxos.spec.js
+++ test/spec/firefoxos.spec.js
@@ -2,28 +2,38 @@
 
 /*global ajax*/
 describe('base api function', function () {
-	it('should have a working test harness', function () {
-		expect(true).not.toBe(false);
+    it('should have a working test harness', function () {
+	expect(true).not.toBe(false);
+    });
+    
+    it('should exist', function () {
+	expect(typeof ajax).toBe('function');
+    });
+    
+    it('should return nothing', function () {
+	var result = ajax('http://mozilla.org', null, function (d, s, x) {
 	});
-
-	it('should exist', function () {
-		expect(typeof ajax).toBe('function');
-	});
-
-	it('should return nothing', function () {
-		var result = ajax('http://mozilla.org', null, function (d, s, x) {
-		});
-		expect(result).toBeUndefined();
-	});
+	expect(result).toBeUndefined();
+    });
 });
 
 describe('makes async requests', function () {
-	it('should return something', function (done) {
-		ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
-			expect(result).not.toBeUndefined();
-			expect(xhr.startedAt).not.toBeUndefined();
-			expect(status).toBe(200);
-			done();
-		});
+    it('should return something', function (done) {
+	ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
+	    expect(result).not.toBeUndefined();
+	    expect(xhr.startedAt).not.toBeUndefined();
+	    expect(status).toBe(200);
+	    done();
 	});
+    });
+    
+    it('should return null', function (done) {
+	ajax('http://mozilla.org', null, function (result, status, xhr) {
+	    console.log(result);
+	    expect(result).toBe(null);
+	    expect(status).toBe(null);
+	    expect(xhr).toBe(null);
+	    done
+	});
+    });
 });
