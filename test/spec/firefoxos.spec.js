5c3b8b19a3d29ff5f12ddcf35a2da3c0668754c5
Scott Anecito <scott.anecito@gmail.com>
2015-01-11 16:09:29 -0800

    Added GET && POST tests, fixed inconsistent coding syntax
    Coding syntax changed was:
    - In some cases only one tab was used, but in the same case, but
    different instance double tabbing is used. Tabbing has now been fixed to
    use double tabbing throughout.
    - There were varying cases of spacing around function calls. For
    example, function () {, function(){, and function() {. Since function ()
    { seemed the most prominent, all cases now use the same syntax for
    consistency.

--- test/spec/firefoxos.spec.js
+++ test/spec/firefoxos.spec.js
@@ -5,32 +5,32 @@
 
 /*global AL*/
 describe('base api function', function () {
-    it('should have a working test harness', function () {
+        it('should have a working test harness', function () {
 		expect(true).not.toBe(false);
-    });
+        });
 
-    it('should exist', function () {
+        it('should exist', function () {
 		expect(typeof AL).toBe('object');
-    });
+        });
 
 	it('should exist', function () {
 		expect(typeof AL.ajax).toBe('function');
-    });
+        });
 
-    it('should return nothing', function () {
+        it('should return nothing', function () {
 		var result = AL.ajax('http://mozilla.org');
 		expect(result).toBeUndefined();
-    });
+        });
 });
 
 describe('makes async requests', function () {
-    it('should return something', function (done) {
+        it('should return something', function (done) {
 		AL.ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
 			expect(result).not.toBeUndefined();
 			expect(status).toBe(200);
 			done();
 		});
-    });
+        });
 
 	it('should return results matching what we are requesting', function (done) {
 		AL.ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
@@ -42,7 +42,7 @@ describe('makes async requests', function () {
 });
 
 describe('gracefully fails with cors blockage', function(){
-	it('should gracefully fail', function(done){
+	it('should gracefully fail', function (done) {
 		AL.ajax('http://php.scripts.psu.edu/dept/liberalarts/sites/kennett/phpinfo.php', null, function(result, status, xhr){
 			expect(status).toBe(0);
 			expect(result.trim()).toBe('');
@@ -52,52 +52,66 @@ describe('gracefully fails with cors blockage', function(){
 });
 
 describe('makes bogus requests', function () {
-    it('should return 404', function (done) {
+        it('should return 404', function (done) {
 		AL.ajax('https://developer.mozilla.org/asdf', null, function (result, status, xhr) {
 			expect(result).not.toBeUndefined();
 			expect(result).not.toBe('NOT FOUND');
 			expect(status).toBe(404);
 			done();
 		});
-    });
+        });
 
-    it('should return 404 and "NOT FOUND"', function (done) {
+        it('should return 404 and "NOT FOUND"', function (done) {
 		AL.ajax('asdfahg.com', null, function (result, status, xhr) {
 			expect(result).toBe('NOT FOUND');
 			expect(status).toBe(404);
 			done();
 		});
-    });
+        });
 });
 
-
 describe('make sure the event is fired when the request happens', function () {
-    it('fires', function (done) {
-   		var listener = function() {
-		  expect(true).toBe(true);
-		  window.removeEventListener('network-ready', listener);  
-		  done();
+        it('fires', function (done) {
+   		var listener = function () {
+                        expect(true).toBe(true);
+                        window.removeEventListener('network-ready', listener);  
+                        done();
 	        }
 		window.addEventListener('network-ready', listener); 
 		
                 AL.ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
                 });
-    });
-});
-
-describe('The latency is recorded', function() {
-    it('is recorded', function(done){
-        AL.ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
-          AL.getLatestAccessTimeStamp(function (result) {
-	    expect(result).not.toBeUndefined();
-            done();  	  
-          });
-	  
         });
-    });
 });
 
+describe('The latency is recorded', function () {
+        it('is recorded', function (done) {
+                AL.ajax('https://developer.mozilla.org/search.json?q=cats', null, function (result, status, xhr) {
+                        AL.getLatestAccessTimeStamp(function (result) {
+                                expect(result).not.toBeUndefined();
+                                done();  	  
+                        });	  
+                });
+        });
+});
 
+describe('verify GET and POST are chosen appropriately', function () {
+        it('should be GET', function (done) {
+                // TODO: Personal URL should be replaced with Server-side URL when server side set up
+                AL.ajax('http://ryan52.info/test.php', null, function (result, status, xhr) {
+                        expect(result).toContain("GET");
+                        done();
+                });
+        });
+
+        it('should be POST', function (done) {
+                // TODO: Personal URL should be replaced with Server-side URL when server side set up
+                AL.ajax('http://ryan52.info/test.php', 'DATA', function (result, status, xhr) {
+                        expect(result).toContain("POST");
+                        done();
+                });
+        });
+});
 
 
 
