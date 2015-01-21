84c002300cae145bb4c0b2956cab1ebb068278e6
Sean <mahan2@pdx.edu>
2015-01-21 11:25:40 -0800

    Non-critical request button and url txt field added to test app

--- examples/battest/app.js
+++ examples/battest/app.js
@@ -1,12 +1,18 @@
 window.addEventListener('load', function () {
 	document.getElementById('fireNetworkBtn').addEventListener('click', fireNetwork);
+	document.getElementById('addNonCritReq').addEventListener('click', addNonCriticalRequest);
 	window.addEventListener('network-ready', updatePendingRequestCount);
 	updatePendingRequestCount();
 	setInterval(updatePendingRequestCount, 100);
 });
 
 function fireNetwork(){
-  window.dispatchEvent(new Event('network-ready'));
+  window.dispatchEvent(new Event('network-ready')); //this is not working, unsure why...
+}
+
+function addNonCriticalRequest(){
+  var urlString = document.getElementById('requestURL');
+	AL.addNonCriticalRequest(urlString.value, null, function (){});
 }
 
 function updatePendingRequestCount() {
