e9506fb639f0857524b5b44e8a4900b9e0a26d72
Casey English <kcpisme@gmail.com>
2015-01-21 19:42:43 -0800

    Continued work on test that non-criticals fire after criticals

--- test/spec/critical.spec.js
+++ test/spec/critical.spec.js
@@ -5,6 +5,11 @@
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 describe('work with last access time stamp', function () {
+	beforeEach(function (done) {
+		if (localforage) {
+			localforage.clear(done);
+		}
+	});
 	afterEach(function (done) {
 		if (localforage) {
 			localforage.clear(done);
@@ -12,16 +17,16 @@ describe('work with last access time stamp', function () {
 	});
 
 	it('should keep last access timestamp or fail gracefully', function (done) {
-		Al.addNonCriticalRequest('https://developer.mozilla.org/search.json?q=dogs', {dogs: 'infinity'}, function () {
-			expect(result).not.toEqual(null);
+		AL.addNonCriticalRequest('https://developer.mozilla.org/search.json?q=dogs', {dogs: 'infinity'}, function (result, status, xhr) {
+			expect(result).not.toBe(null);
 			AL.getHistory(function (history) {
 				if (localforage) {
 					expect(history).not.toBe(null);
-					expect(history.length).toBe(2);
+					expect(history[0]['end'].toString()).toBe(2);
 				}
 			});
 		});
-		AL.ajax('https://developer.mozilla.org/search.json?q=cats', {cats: 'infinity'}, function () {
+		AL.ajax('https://developer.mozilla.org/search.json?q=cats', {cats: 'infinity'}, function (result, status, xhr) {
 			expect(result).not.toEqual(null);
 			AL.getHistory(function (history) {
 				if (localforage) {
@@ -30,10 +35,10 @@ describe('work with last access time stamp', function () {
 				} else {
 					expect(history).toBe(null);
 				}
-				done();
 			});
 			
 		});
+		done();
 	});
 });
 
