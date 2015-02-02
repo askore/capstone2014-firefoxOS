837d8d2a96c8b411a9b23303a70d11aaf514bb29
Casey English <kcpisme@gmail.com>
2015-02-02 15:44:21 -0800

    Trying to get travis to sftp app to server

--- examples/tests/test.js
+++ examples/tests/test.js
@@ -2,12 +2,12 @@ module.exports = {
 	"Battest App Tests" : function (browser) {
 		browser
 			.url("http://54.213.122.61/capstone/index.html")
-			.waitForElementVisible('body', 2000)
-			.waitForElementVisible('button[id=clearHistory]', 2000)
+			.waitForElementVisible('body', 1000)
+			.waitForElementVisible('button[id=clearHistory]', 1000)
 			.click('button[id=clearHistory]')
 			.waitForElementVisible('button[id=fireCriticalReq]', 1000)
 			.click('button[id=fireCriticalReq]')
-			.pause(2000)
+			.pause(1000)
 			.assert.containsText('span[id=totalRequestCount]', '2')
 			.end();
 	}
