5e35e44e4a0f4351c0c48c951ea8625f79d7c1d8
Casey English <kcpisme@gmail.com>
2015-02-02 14:53:34 -0800

    Seeing if increasing timeout works

--- examples/tests/test.js
+++ examples/tests/test.js
@@ -2,12 +2,12 @@ module.exports = {
 	"Battest App Tests" : function (browser) {
 		browser
 			.url("http://54.213.122.61/capstone/index.html")
-			.waitForElementVisible('body', 1000)
-			.waitForElementVisible('button[id=clearHistory]', 1000)
+			.waitForElementVisible('body', 2000)
+			.waitForElementVisible('button[id=clearHistory]', 2000)
 			.click('button[id=clearHistory]')
 			.waitForElementVisible('button[id=fireCriticalReq]', 1000)
 			.click('button[id=fireCriticalReq]')
-			.pause(1000)
+			.pause(2000)
 			.assert.containsText('span[id=totalRequestCount]', '2')
 			.end();
 	}
