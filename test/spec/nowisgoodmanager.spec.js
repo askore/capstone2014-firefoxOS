2391afccda091bef7184286345276c152c73c60b
Nathan Kerr <nathankerr@gmail.com>
2015-02-05 18:53:59 -0800

    Fire checkIfGood if it hasn't in the last 15 minutes. Only good if battery level is above 10%

--- test/spec/nowisgoodmanager.spec.js
+++ test/spec/nowisgoodmanager.spec.js
@@ -3,7 +3,7 @@
 /*global AL*/
 
 describe('manager detects critical request fired', function(){
-	iit('should dispatch event for non-critical requests', function(done){
+	it('should dispatch event for non-critical requests', function(done){
 		window.addEventListener('network-ready', done, true);
 		window.dispatchEvent(new Event('critical-request-success'));
 	});
